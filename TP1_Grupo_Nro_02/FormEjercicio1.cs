9c627565b6b8ff7a0a01ea68a402980ef2eb6b7c
ElvisPastafrolo <guillermoserafini61@gmail.com>
2024-08-13 14:25:34 -0300

    Add files via upload
    El ejercicio 1 ya no permite ingresar 2 nombres iguales, ya sea en minuscula o mayuscula

--- TP1_Grupo_Nro_02/FormEjercicio1.cs
+++ TP1_Grupo_Nro_02/FormEjercicio1.cs
@@ -19,15 +19,31 @@ namespace TP1_Grupo_Nro_02
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ') ///IsLetter determina que la key presionada sea una letra Ma o Mi,
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != ' ') ///Controla si la tecla presionada es una letra, es un espacio o es la de retroceso
                 e.Handled = true;
         }
 
+        private void NombreValido()
+        {
+            bool repetido = false;
+
+            for (int i = 0; i < listBox1.Items.Count; i++){
+                string nombreaux = textBox1.Text.ToUpper(); ///crea una cadena auxiliar igual al txtbox pero en mayusculas
+                if (nombreaux == listBox1.Items[i].ToString().ToUpper()) ///compara todas las cadenas de la lstbox pasandolas a mayusculas
+                    repetido = true;    ///si se repite alguno, el bool repetido pasa a true
+            }
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && repetido == false){    ///IsNullOrWhiteSpace controla que no haya solo un espacio en blanco y repetido, que el nombre no se encuentre ya en uso
+                listBox1.Items.Add(textBox1.Text.Trim()); ///Copia lo del txtbox1 a la listbox
+                textBox1.Clear();
+            }
+
+        }
+
+
+
         private void btnagregar_Click(object sender, EventArgs e)
         {
-            if(!string.IsNullOrWhiteSpace(textBox1.Text))
-            listBox1.Items.Add(textBox1.Text.Trim());
-            textBox1.Clear();
+            NombreValido();
         }
     }
 }
