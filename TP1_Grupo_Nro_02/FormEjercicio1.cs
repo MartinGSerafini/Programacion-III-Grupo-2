45f73e8e5eba4b5cdbb05b620931eb35bbfcc32e
ElvisPastafrolo <guillermoserafini61@gmail.com>
2024-08-13 14:42:59 -0300

    Add files via upload
    La estructura del formulario principal esta hecha.
    La estructura del formulario Ejercicio 1 esta hecha.
    El punto 1 no permite ingresar nombres repetidos, numeros,ni texto "vacio" y el boton aceptar ya copia los nombres a la Listbox1

---
+++ TP1_Grupo_Nro_02/FormEjercicio1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_Grupo_Nro_02
+{
+    public partial class FormEjercicio1 : Form
+    {
+        public FormEjercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != ' ') ///Controla si la tecla presionada es una letra, es un espacio o es la de retroceso
+                e.Handled = true;
+        }
+
+        private void NombreValido() ///Valida que el nombre ingresado sea valido
+        {
+            bool repetido = false;
+
+            for (int i = 0; i < ListBox1.Items.Count; i++){
+                string nombreaux = TextBox1.Text.ToUpper(); ///crea una cadena auxiliar igual al txtbox pero en mayusculas
+                if (nombreaux == ListBox1.Items[i].ToString().ToUpper()) ///compara todas las cadenas de la lstbox pasandolas a mayusculas
+                    repetido = true;    ///si se repite alguno, el bool repetido pasa a true
+            }
+            if (!string.IsNullOrWhiteSpace(TextBox1.Text) && repetido == false){    ///IsNullOrWhiteSpace controla que no haya solo un espacio en blanco y repetido, que el nombre no se encuentre ya en uso
+                ListBox1.Items.Add(TextBox1.Text.Trim()); ///Copia lo del txtbox1 a la listbox
+                TextBox1.Clear();
+            }
+
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            NombreValido(); ///llama a la funcion que se encarga de validar el nombre
+        }
+    }
+}
---
+++ TP1_Grupo_Nro_02/FormPrincipal.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TP1_Grupo_Nro_02
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btnejercicio1 = new System.Windows.Forms.Button();
+            this.Btnejercicio2 = new System.Windows.Forms.Button();
+            this.Btnejercicio3 = new System.Windows.Forms.Button();
+            this.Lblintegrantes = new System.Windows.Forms.Label();
+            this.Lblintegrantes2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btnejercicio1
+            // 
+            this.Btnejercicio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btnejercicio1.Location = new System.Drawing.Point(25, 51);
+            this.Btnejercicio1.Name = "Btnejercicio1";
+            this.Btnejercicio1.Size = new System.Drawing.Size(120, 42);
+            this.Btnejercicio1.TabIndex = 0;
+            this.Btnejercicio1.Text = "EJERCICIO 1";
+            this.Btnejercicio1.UseVisualStyleBackColor = true;
+            this.Btnejercicio1.Click += new System.EventHandler(this.Btnejercicio1_Click);
+            // 
+            // Btnejercicio2
+            // 
+            this.Btnejercicio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btnejercicio2.Location = new System.Drawing.Point(151, 51);
+            this.Btnejercicio2.Name = "Btnejercicio2";
+            this.Btnejercicio2.Size = new System.Drawing.Size(120, 42);
+            this.Btnejercicio2.TabIndex = 1;
+            this.Btnejercicio2.Text = "EJERCICIO 2";
+            this.Btnejercicio2.UseVisualStyleBackColor = true;
+            this.Btnejercicio2.Click += new System.EventHandler(this.Btnejercicio2_Click);
+            // 
+            // Btnejercicio3
+            // 
+            this.Btnejercicio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btnejercicio3.Location = new System.Drawing.Point(277, 51);
+            this.Btnejercicio3.Name = "Btnejercicio3";
+            this.Btnejercicio3.Size = new System.Drawing.Size(120, 42);
+            this.Btnejercicio3.TabIndex = 2;
+            this.Btnejercicio3.Text = "EJERCICIO 3";
+            this.Btnejercicio3.UseVisualStyleBackColor = true;
+            this.Btnejercicio3.Click += new System.EventHandler(this.Btnejercicio3_Click);
+            // 
+            // Lblintegrantes
+            // 
+            this.Lblintegrantes.AutoSize = true;
+            this.Lblintegrantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblintegrantes.Location = new System.Drawing.Point(27, 122);
+            this.Lblintegrantes.Name = "Lblintegrantes";
+            this.Lblintegrantes.Size = new System.Drawing.Size(99, 20);
+            this.Lblintegrantes.TabIndex = 3;
+            this.Lblintegrantes.Text = "Integrantes: ";
+            // 
+            // Lblintegrantes2
+            // 
+            this.Lblintegrantes2.AutoSize = true;
+            this.Lblintegrantes2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblintegrantes2.Location = new System.Drawing.Point(97, 152);
+            this.Lblintegrantes2.Name = "Lblintegrantes2";
+            this.Lblintegrantes2.Size = new System.Drawing.Size(246, 120);
+            this.Lblintegrantes2.TabIndex = 4;
+            this.Lblintegrantes2.Text = " - Martin Guillermo Serafini\r\n- Luciano Cogo\r\n- Fabrizio Alejandro De Rui Sartor\r" +
+    "\n- Elen Ignacio Saucedo\r\n- Juan Martin Monte\r\n- Sofia Pilar Vitalevi";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 321);
+            this.Controls.Add(this.Lblintegrantes2);
+            this.Controls.Add(this.Lblintegrantes);
+            this.Controls.Add(this.Btnejercicio3);
+            this.Controls.Add(this.Btnejercicio2);
+            this.Controls.Add(this.Btnejercicio1);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btnejercicio1;
+        private System.Windows.Forms.Button Btnejercicio2;
+        private System.Windows.Forms.Button Btnejercicio3;
+        private System.Windows.Forms.Label Lblintegrantes;
+        private System.Windows.Forms.Label Lblintegrantes2;
+    }
+}
+
