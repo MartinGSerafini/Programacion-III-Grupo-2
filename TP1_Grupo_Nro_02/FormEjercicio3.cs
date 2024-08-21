7cf039384e5cc37d7dd3c4ce262bbc2a354489db
Pily <75502236+Piiily@users.noreply.github.com>
2024-08-21 14:03:53 -0300

    Update oficios
    Se agrega la opción de desempleado u otro.

--- TP1_Grupo_Nro_02/FormEjercicio3.cs
+++ TP1_Grupo_Nro_02/FormEjercicio3.cs
@@ -43,6 +43,20 @@ namespace TP1_Grupo_Nro_02
                 string Aux = Clbox.CheckedItems[i].ToString(); // Obtiene el elemento seleccionado
                 Label1.Text += "\r\n" + "   -" + Aux;  //Lo muestra 
             }
+
+            DialogResult respuesta = DialogResult.Cancel;
+            if (Clbox.CheckedItems.Count == 0)//Para el caso de no haber seleccionado un oficio
+            {
+               respuesta = MessageBox.Show("No ah seleccionado ningún oficio. \r\n Tiene otro oficio? Seleccione \"Si\" o \"No\". \r\n Si quiere volver a seleccionar un oficio seleccione \"Cancelar\"", "Selección vacía", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            }
+            
+            if (respuesta == DialogResult.Yes)
+            {
+               Label1.Text += "\r\n" + "   -Otro";
+            }
+            else if (respuesta == DialogResult.No) {
+            Label1.Text += "\r\n" + "   -Desempleado";
+            }
         }
 
         private void Btn_Click(object sender, EventArgs e)
