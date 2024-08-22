4d8ee22489ae47fec4d0cde773ee118246c98942
Anastasio23 <137455854+Anastasio23@users.noreply.github.com>
2024-08-22 12:04:30 -0300

    Add files via upload

--- TP2_Grupo_Nro_02/Ejercicio2A.aspx.cs
+++ TP2_Grupo_Nro_02/Ejercicio2A.aspx.cs
@@ -26,6 +26,16 @@ namespace TP2_Grupo_Nro_XX
 
         protected void Btnverresumen_Click(object sender, EventArgs e)
         {
+            
+            string selectedItems = "";
+            foreach (ListItem item in CheckBoxList1.Items)
+            {
+                if (item.Selected)
+                {
+                    selectedItems += item.Text + "<br />"; // Puedes cambiar el delimitador si lo prefieres
+                }
+            }
+            Session["SelectedItems"] = selectedItems;
             Server.Transfer("Ejercicio2B.aspx");
         }
     }
--- TP2_Grupo_Nro_02/Ejercicio2B.aspx.cs
+++ TP2_Grupo_Nro_02/Ejercicio2B.aspx.cs
@@ -14,14 +14,18 @@ namespace TP2_Grupo_Nro_XX
             string Nombre;
             string Apellido;
             string Zona;
+            string selectedItems = Session["SelectedItems"] as string;
 
             Nombre = Request["Txbnombre"].ToString();
             Apellido = Request["Txbapellido"].ToString();
             Zona = Request["Ddlzona"].ToString();
 
+            
+
             lblNombre.Text = "Nombre: " + Nombre;
             lblApellido.Text = "Apellido: " + Apellido;
             lblZona.Text = "Zona: " + Zona;
+            lblTemas.Text = "Temas seleccionados: " + "<br />" + selectedItems;
         }
     }
 }
