0b2242e14b8e2bab6c10c13e7bdbe50094896358
Pily <75502236+Piiily@users.noreply.github.com>
2024-09-14 11:46:44 -0300

    Se conecta a la tabla temas el ddl de temas

--- TP4_Grupo_Nro_02/Programacion3-Grupo2-TP4/Ejercicio3a.aspx.cs
+++ TP4_Grupo_Nro_02/Programacion3-Grupo2-TP4/Ejercicio3a.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Runtime.Remoting.Messaging;
 
 namespace Programacion3_Grupo2_TP4
 {
@@ -11,7 +14,23 @@ namespace Programacion3_Grupo2_TP4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(IsPostBack == false)
+            {
+                using (SqlConnection bdLibreria = new SqlConnection("Data Source=PILY\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True"))
+                {
+                    bdLibreria.Open();
 
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Temas", bdLibreria);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    ddlTemas.DataSource = reader;
+                    ddlTemas.DataTextField = "Tema";
+                    ddlTemas.DataValueField = "idTema";
+                    ddlTemas.DataBind();
+
+                    ddlTemas.Items.Insert(0, new ListItem("Seleccione un tema", ""));
+                }
+            }
         }
     }
 }
