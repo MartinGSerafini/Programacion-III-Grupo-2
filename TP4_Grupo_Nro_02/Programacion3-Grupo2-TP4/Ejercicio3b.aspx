41c389236531c3deb8e9a62fb9283daca56f766a
Anastasio23 <137455854+Anastasio23@users.noreply.github.com>
2024-09-14 10:30:24 -0300

    Add files via upload

---
+++ TP4_Grupo_Nro_02/Programacion3-Grupo2-TP4/Ejercicio3b.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio3b.aspx.cs" Inherits="Programacion3_Grupo2_TP4.Ejercicio3b" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblLibros" runat="server" Font-Bold="True" Text="Listado de Libros:"></asp:Label>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:LinkButton ID="lbtnConsultar" runat="server">Consultar otro tema</asp:LinkButton>
+    </form>
+</body>
+</html>
---
+++ TP4_Grupo_Nro_02/Programacion3-Grupo2-TP4/Ejercicio3b.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Programacion3_Grupo2_TP4
+{
+
+
+    public partial class Ejercicio3b
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblLibros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLibros;
+
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// Control lbtnConsultar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbtnConsultar;
+    }
+}
