d1ab9d70f68edac03efba744471d190b7b9de90b
Martin guillermo serafini <guillermoserafini61@gmail.com>
2024-09-30 09:47:34 -0300

    .

---
+++ TP6_Grupo_Nro_02/TP6_Grupo_Nro_02/Ejercicio 1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP6_Grupo_Nro_02
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string consulta = "SELECT IdProducto AS [Id Producto], NombreProducto AS [Nombre Producto], CantidadPorUnidad AS [Cantidad Por Unidad], PrecioUnidad AS [Precio Unidad] FROM Productos";
+            ConexionSQL conexion = new ConexionSQL();
+            conexion.consultaGrd(consulta,GrdProductos);
+        }
+    }
+}
