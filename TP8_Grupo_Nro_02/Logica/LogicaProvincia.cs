3150bb2771331c3f791f89f50ddf59cb488241ed
Pilar Vitalevi <sofiapilarvitalevi@gmail.com>
2024-10-17 10:01:35 -0300

    .

---
+++ TP8_Grupo_Nro_02/Logica/LogicaProvincia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Logica
+{
+    public class LogicaProvincia
+    {
+        public DataTable getTabla()
+        {
+            DaoProvincia dao = new DaoProvincia();
+            return dao.getTablaProvincia();
+        }
+
+        public Provincia getProvincia(int id)
+        {
+            DaoProvincia dao = new DaoProvincia();
+            Provincia prov = new Provincia();
+            prov.setId_Provincia(id);
+            return dao.getProvincia(prov);
+        }
+    }
+}
