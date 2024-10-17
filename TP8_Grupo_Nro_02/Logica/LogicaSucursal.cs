a3f4d19099d15cbfcc7ba23c0d2c92cb094c7c7c
Pilar Vitalevi <sofiapilarvitalevi@gmail.com>
2024-10-17 13:13:11 -0300

    .

---
+++ TP8_Grupo_Nro_02/Logica/LogicaSucursal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Logica
+{
+    public class LogicaSucursal
+    {
+        public DataTable getTabla()
+        {
+            DaoSucursal dao = new DaoSucursal();
+            return dao.getTablaSucursales();
+        }
+
+        public Sucursal get(int id)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal sucu = new Sucursal();
+            sucu.setId_Sucursal(id);
+            return dao.getSucursal(sucu);
+        }
+
+        public bool eliminarSucursal(int id)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal sucu = new Sucursal();
+            sucu.setId_Sucursal(id);
+            int op = dao.eliminarSucursal(sucu);
+            if (op == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool agregarSucursal(String nombre)
+        {
+            int cantFilas = 0;
+
+            Sucursal sucu = new Sucursal();
+            sucu.setNombreSucursal(nombre);
+
+            DaoSucursal dao = new DaoSucursal();
+            if (dao.existeSucursal(sucu) == false)
+            {
+                cantFilas = dao.agregarSucursal(sucu);
+            }
+
+            if (cantFilas == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
