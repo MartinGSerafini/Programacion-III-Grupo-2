35eef9da97f3b4567b74e40b2c1aafbbd48c3618
Pilar Vitalevi <sofiapilarvitalevi@gmail.com>
2024-10-17 09:59:57 -0300

    .

---
+++ TP8_Grupo_Nro_02/Entidades/Provincia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Provincia
+    {
+        private int Id_Provincia;
+        private string DescripcionProvincia;
+
+        public Provincia() { }
+
+        public int getId_Provincia() {return Id_Provincia;}
+        public void setId_Provincia(int IdProvincia) { Id_Provincia = IdProvincia;}
+        public string getDescripcionProvincia() { return DescripcionProvincia;}
+        public void setDescripcionCategoria(string Descripcion) { DescripcionProvincia = Descripcion;}
+    }
+}
