09a8441d93e440c148ccca4859776c92adec974c
Fabrizio Alejandro De Rui Sartor <fabrizioderuisartor@gmail.com>
2024-11-09 00:05:49 -0300

    Agregando la LogicaPacientes

---
+++ TPINT_GRUPO_02_PR3/Logica/LogicaPacientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Logica
+{
+    public class LogicaPacientes
+    {
+        public DataTable getTabla()
+        {
+            DaoPacientes dao = new DaoPacientes();
+            return dao.getTablaPacientes();
+        }
+
+        public DataTable getTablaFiltrada(string dato, string filtro)
+        {
+            DaoPacientes dao = new DaoPacientes();
+            return dao.getTablaPacientesFiltrada(filtro, dato);
+        }
+    }
+}
